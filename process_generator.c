4cd0ae43278bce1ae1d4b75d17167a294efe0063
ZiadHF <ziadhfahmy@yahoo.com>
2024-04-26 15:53:07 +0300

    added process_generator.c and some other changes

--- process_generator.c
+++ process_generator.c
@@ -1,27 +1,99 @@
 #include "headers.h"
+#define IGNORE_LENGTH 128
+#define MAX_SIZE 1024
+int numOfProcesses = 0;
+struct process** processQueue = NULL;
+
+void testerfunction(struct process* p){
+    printf("%d %d %d %d %d",p->id,p->arrivaltime,p->runningtime,p->remainingtime,p->priority);
+    printf("\n");
+}
+
+void skipLine(FILE* f){
+    char ignore[IGNORE_LENGTH];
+    fgets(ignore,IGNORE_LENGTH,f);
+}
+int getnoOfProcesses(FILE* f){
+    int c;
+    int count = 0;
+    while ((c = fgetc(f)) != EOF) { if (c == '\n') { count++; } }
+    fseek(f,0,SEEK_SET);
+    return count;
+}
 
 void clearResources(int);
 
 int main(int argc, char * argv[])
 {
     signal(SIGINT, clearResources);
-    // TODO Initialization
-    // 1. Read the input files.
-    // 2. Ask the user for the chosen scheduling algorithm and its parameters, if there are any.
-    // 3. Initiate and create the scheduler and clock processes.
-    // 4. Use this function after creating the clock process to initialize clock
+    FILE* f = fopen("processes.txt","r");
+    if (f == NULL){
+        perror("Error opening file");
+        return -1;
+    }
+    skipLine(f);
+    numOfProcesses = getnoOfProcesses(f);
+    skipLine(f);
+    int SchedAlgo,RR_Quantum = 0,i = 0;
+    printf("Enter the scheduling algorithm you want: (1 for Round Robin, 2 for xxxx, 3 for xxxx) ");
+    scanf("%d", &SchedAlgo);
+    if (SchedAlgo == 1){
+        printf("Enter your Round Robin Quantum: ");
+        scanf("%d",&RR_Quantum);
+    }
+    processQueue = (struct process**)malloc(numOfProcesses * sizeof(struct process));
+    char line[MAX_SIZE]; 
+    while (fgets(line, sizeof(line), f) != NULL){
+        int ID,AT,RunningTime,PRI;//,MEMSISZE;
+        sscanf(line,"%d %d %d %d", &ID,&AT,&RunningTime,&PRI);               //,&MEMSIZE);
+        processQueue[i++] = initializeProcess(ID,AT,RunningTime,PRI);    //,MEMSIZE)
+        testerfunction(processQueue[i-1]);
+    }
+    fclose(f);
+    i = 0;
+    char* SchedParam[4]; // TO BE ADDED
+    switch (SchedAlgo){
+        case 1:
+            SchedParam[0] = "RR";
+            break;
+        case 2:
+            SchedParam[0] = "SRTN";
+            break;
+        case 3:
+            SchedParam[0] = "HPF";
+            break;
+        default:
+            perror("Invalid Scheduling Algorithm");
+            return 1;
+            break;
+    }
+    char numOfProc[5];
+    sprintf(numOfProc,"%d",numOfProcesses);
+    SchedParam[1] = numOfProc;
+    SchedParam[2] = RR_Quantum == 0 ? "0" : "1";
+    SchedParam[3] = NULL;
+    pid_t Clock = fork();
+    if (Clock == 0){ execv("./clk.o",NULL); }
     initClk();
-    // To get time use this
-    int x = getClk();
-    printf("current time is %d\n", x);
-    // TODO Generation Main Loop
-    // 5. Create a data structure for processes and provide it with its parameters.
-    // 6. Send the information to the scheduler at the appropriate time.
-    // 7. Clear clock resources
-    destroyClk(true);
+    pid_t Scheduler = fork();
+    if (Scheduler == 0){ execv("./scheduler.o",SchedParam); }
+    int msgid;
+    while (i < numOfProcesses){
+        if (getClk() == processQueue[i]->arrivaltime){
+            msgsnd(msgid, processQueue[i], sizeof(struct process),IPC_NOWAIT);
+            printf("Process %d sent to scheduler at time = %d\n",processQueue[i]->id,processQueue[i]->arrivaltime);
+            i++;
+        }
+    }
+    int stat_loc;
+    //waitpid(Scheduler,&stat_loc,0);
+    clearResources(0);
 }
 
 void clearResources(int signum)
 {
     //TODO Clears all resources in case of interruption
+    for (int i = 0 ; i < numOfProcesses ; i++){ free(processQueue[i]); }
+    free(processQueue);
+    destroyClk(true);
 }
